ee2c9dac8946c0af8a418e8abca1d84cae618bae
Mikhael <msantos@truora.com>
2022-01-31 15:10:10 -0400

    Reduce the epochs in the train model script

--- train_model.sh
+++ train_model.sh
@@ -4,4 +4,10 @@ base_dir="segmentation"
 
 cd "$PWD/$base_dir/detr"
 
-python3 main.py --dataset_file face --data_path ../dataset/ --output_dir output --device cuda --resume weights/detr-r50-e632da11.pth
+python3 main.py \
+  --dataset_file face
+  --data_path ../dataset/ \
+  --epochs 2
+  --output_dir output \
+  --device cuda \
+  --resume weights/detr-r50-e632da11.pth
