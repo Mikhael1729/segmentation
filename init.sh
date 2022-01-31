c026577cdae14d9d868295d2bfb737f5203e485e
Mikhael <msantos@truora.com>
2022-01-31 01:13:32 -0400

    Add init script

---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+dataset_ids=("15hGDLhsx8bLgLcIRD5DhYt5iBxnjNF1M" "1GUCogbp16PMGa39thoMMeWxp7Rp5oM8Q" "1HIfDbVEWKmsYKJZm4lchTBDLW5N7dY5T" "1IgM1-ctR6FsveYNZhnh9xSCW_Gucap0a")
+dataset_names=("WIDER_train.zip" "WIDER_val.zip" "WIDER_test.zip"  "wider_face_split.zip")
+dataset_dir="dataset"
+
+mkdir $dataset_dir
+cd $dataset_dir
+
+for index in "${!dataset_ids[@]}"
+do
+  dataset_name=${dataset_names[$index]}
+
+  echo "---[ Downloading $dataset_name ]---"
+
+  gdown --id ${dataset_ids[$index]} --output .
+
+  echo "---[ Unzipping $dataset_name ]---"
+  unzip $dataset_name -d .
+done
